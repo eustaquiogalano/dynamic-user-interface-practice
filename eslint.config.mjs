8fa52ab0abe2c749f96b59b37cc7eda4440b2e72
eustaquio <135113924+eustaquiogalano@users.noreply.github.com>
2025-05-06 16:29:56 +0800

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  {
+    extends: ["prettier"],
+  },
+]);

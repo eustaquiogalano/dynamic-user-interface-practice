65d0a78328c7352776f1c35dd42a9a6b2e94b7ea
eustaquio <eustaquio.galano@gmail.com>
2025-05-13 11:59:05 +0800

    feat: add event listener to each circle button

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,3 +1,11 @@
+import {
+  changeToImage1,
+  changeToImage2,
+  changeToImage3,
+  changeToImage4,
+  changeToImage5,
+} from './image_changer';
+
 export function attachNavButtonClickEvent() {
   // Get a reference of menu button
   document
@@ -6,4 +14,12 @@ export function attachNavButtonClickEvent() {
     .addEventListener('click', () => {
       document.querySelector('.nav-main').classList.toggle('toggle-display');
     });
+}
+
+export function attachCircleButtonClickEvent() {
+  document.querySelector('#circle-1').addEventListener('click', changeToImage1);
+  document.querySelector('#circle-2').addEventListener('click', changeToImage2);
+  document.querySelector('#circle-3').addEventListener('click', changeToImage3);
+  document.querySelector('#circle-4').addEventListener('click', changeToImage4);
+  document.querySelector('#circle-5').addEventListener('click', changeToImage5);
 }

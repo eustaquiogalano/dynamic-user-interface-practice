2b106e4bf50acf5e8a335c52dc0ee2a0b689f683
eustaquio <eustaquio.galano@gmail.com>
2025-05-15 11:38:12 +0800

    refactor: move related functions inside an object

--- src/modules/image-navigator.js
+++ src/modules/image-navigator.js
@@ -4,22 +4,22 @@ import image3 from '../images/image-3.jpg';
 import image4 from '../images/image-4.jpg';
 import image5 from '../images/image-5.jpg';
 
-export function changeToImage1() {
-  document.querySelector('.image').src = image1;
-}
+const circleButtonNavigator = {
+  changeToImage1() {
+    document.querySelector('.image').src = image1;
+  },
+  changeToImage2() {
+    document.querySelector('.image').src = image2;
+  },
+  changeToImage3() {
+    document.querySelector('.image').src = image3;
+  },
+  changeToImage4() {
+    document.querySelector('.image').src = image4;
+  },
+  changeToImage5() {
+    document.querySelector('.image').src = image5;
+  },
+};
 
-export function changeToImage2() {
-  document.querySelector('.image').src = image2;
-}
-
-export function changeToImage3() {
-  document.querySelector('.image').src = image3;
-}
-
-export function changeToImage4() {
-  document.querySelector('.image').src = image4;
-}
-
-export function changeToImage5() {
-  document.querySelector('.image').src = image5;
-}
+export { circleButtonNavigator };

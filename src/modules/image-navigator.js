a2e061a115288bf9f37496826a7f6f7c7fec744a
eustaquio <eustaquio.galano@gmail.com>
2025-05-15 13:54:38 +0800

    refactor: add global variable image tracker

--- src/modules/image-navigator.js
+++ src/modules/image-navigator.js
@@ -4,21 +4,28 @@ import image3 from '../images/image-3.jpg';
 import image4 from '../images/image-4.jpg';
 import image5 from '../images/image-5.jpg';
 
+let imageNumberTracker = 1;
+
 const circleButtonNavigator = {
   changeToImage1() {
     document.querySelector('.image').src = image1;
+    imageNumberTracker = 1;
   },
   changeToImage2() {
     document.querySelector('.image').src = image2;
+    imageNumberTracker = 2;
   },
   changeToImage3() {
     document.querySelector('.image').src = image3;
+    imageNumberTracker = 3;
   },
   changeToImage4() {
     document.querySelector('.image').src = image4;
+    imageNumberTracker = 4;
   },
   changeToImage5() {
     document.querySelector('.image').src = image5;
+    imageNumberTracker = 5;
   },
 };
 
